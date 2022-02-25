e586c27dfcca5473581eff6fd9162fba311ec312
tranduchuy682 <55192155+tranduchuy682@users.noreply.github.com>
2022-02-25 22:23:20 +0700

    update create_data_lists

---
+++ create_data_lists_colab.py
@@ -0,0 +1,8 @@
+# """"for colab""""
+from utils import create_data_lists
+
+create_data_lists(voc07_path='/content/learn_computer_vision/pytorch/architecture/SSD/VOC2007',
+                  voc12_path='/content/learn_computer_vision/pytorch/architecture/SSD/VOC2012',
+                  output_folder='/content/learn_computer_vision/pytorch/architecture/SSD')
+print("done create_data_lists.py")
+
---
+++ create_data_lists_kaggle.py
@@ -0,0 +1,15 @@
+# """"for colab""""
+# from utils import create_data_lists
+
+# create_data_lists(voc07_path='/content/learn_computer_vision/pytorch/architecture/SSD/VOC2007',
+#                   voc12_path='/content/learn_computer_vision/pytorch/architecture/SSD/VOC2012',
+#                   output_folder='/content/learn_computer_vision/pytorch/architecture/SSD')
+# print("done create_data_lists.py")
+
+
+# """"for kaggle""""
+from utils import create_data_lists
+
+create_data_lists('./AllDatabase',
+                  './')
+print("done create_data_lists.py")
